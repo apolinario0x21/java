edcf30e2af35d49b368be774148436b4368a48ee
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-05-22 00:58:30 -0300

    add ex import

---
+++ exercises/src/Ex05_Import.java
@@ -0,0 +1,10 @@
+package exercises.src;
+
+import java.util.Date;
+
+public class Ex05_Import {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
